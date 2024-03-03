de9383dca066c62ad04b9866b815aeeeefba6862
PrikolistTheSillyThing <150598809+PrikolistTheSillyThing@users.noreply.github.com>
2024-03-03 14:38:21 +0200

    hourly payment ready!

---
+++ age-calculator.js
@@ -0,0 +1,16 @@
+// let currentYear = new Date().getFullYear()
+// let birthYear   = prompt("enter your year of birth")
+
+// let age = currentYear - birthYear
+
+// alert (age)
+
+
+    // salary calculation 
+
+      
+    let hoursOfWork = prompt("enter the amount of hours you spend working")
+    let hourlyPayment = 10 
+
+    let salary = hoursOfWork * 10 
+    alert (salary)
