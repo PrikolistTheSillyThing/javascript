df6296053dbf6bd77a38610900c137eb41abaaa1
PrikolistTheSillyThing <150598809+PrikolistTheSillyThing@users.noreply.github.com>
2024-03-07 15:51:21 +0200

    operators ready!

---
+++ student.js
@@ -0,0 +1,10 @@
+let studentGeneralMark        = 6.5
+let studentBudget             = 5_000
+let studentConnections        = true
+
+const UNIVERSTITY_GRADE       = 7.0
+const UNIVERSTITY_CONTRACT    = 40_000
+const UNIVERSTITY_CONNECTIONS = true
+
+let approve = studentGeneralMark >= UNIVERSTITY_GRADE || studentBudget >= UNIVERSTITY_CONTRACT || studentConnections == UNIVERSTITY_CONNECTIONS
+
