33f0940cb60a5e391ae68a3c38af0577f85488b2
PrikolistTheSillyThing <150598809+PrikolistTheSillyThing@users.noreply.github.com>
2024-04-22 22:32:25 +0300

    object ready!

---
+++ object.js
@@ -0,0 +1,19 @@
+let series = {
+    name: "Breaking Bad",
+    genre: "Thriller",
+    releaseDate: 2008
+}
+
+let book = {
+    name: "Hobbit",
+    author: "John Ronald Reuel Tolkien",
+    genre: "Fantasy"
+}
+
+let game = {
+    name: "Half-Life",
+    releaseDate: 1998,
+    creators: "Valve Software",
+    publishers: "Valve Software",
+    genre: "First Person Shooter"
+}
