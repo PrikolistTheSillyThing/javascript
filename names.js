5a29be122a326443480477b616a1391fc52f6843
PrikolistTheSillyThing <150598809+PrikolistTheSillyThing@users.noreply.github.com>
2024-04-22 22:30:47 +0300

    names ready

---
+++ names.js
@@ -0,0 +1,25 @@
+let names = [
+    "John Doe",
+    "Marry Poppins",
+    "Pete Cork",
+    "Jake Solomon",
+    "Vasilii Sviatoi",
+]
+
+ for (let i=1; i<4; i++) {
+     console.log( names[i] )
+ }
+
+// let nameToFind = prompt("Name to find: ")
+
+// for (let i=0; i<5; i++) {
+//     if (names[i] == nameToFind) {
+//         console.log(`${nameToFind} found!`)
+//     }
+// }
+
+debugger
+
+for (let i=0; i<5; i++) {
+    list.innerHTML += `<li> ${names[i]}`
+}
